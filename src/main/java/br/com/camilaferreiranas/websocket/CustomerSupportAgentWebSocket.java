9e617d76d9333cd761cdeb9bdc624be9334f3e55
Camila Ferreira <camila_fn97@hotmail.com>
2025-10-23 20:31:25 -0300

    feat: initial commit

---
+++ src/main/java/br/com/camilaferreiranas/websocket/CustomerSupportAgentWebSocket.java
@@ -0,0 +1,26 @@
+package br.com.camilaferreiranas.websocket;
+
+import br.com.camilaferreiranas.ia.CustomerSupportAgent;
+import io.quarkus.websockets.next.OnOpen;
+import io.quarkus.websockets.next.OnTextMessage;
+import io.quarkus.websockets.next.WebSocket;
+
+@WebSocket(path = "/customer-support-agent")
+public class CustomerSupportAgentWebSocket {
+
+    private final CustomerSupportAgent customerSupportAgent;
+
+    public CustomerSupportAgentWebSocket(CustomerSupportAgent customerSupportAgent) {
+        this.customerSupportAgent = customerSupportAgent;
+    }
+
+    @OnOpen
+    public String onOpen() {
+        return "Welcome to Miles of Smiles! How can I help you today?";
+    }
+
+    @OnTextMessage
+    public String onTextMessage(String message) {
+        return customerSupportAgent.chat(message);
+    }
+}
